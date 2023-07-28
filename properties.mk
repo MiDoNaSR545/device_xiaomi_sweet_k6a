68ed14a46393c642f5b60b53bbf7b5e6c040726a
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-07-28 06:37:41 +0300

    sweet_k6a: Enable QCRIL radio power saving
    Signed-off-by: UnmoveD <unmoved2121@gmail.com>

--- properties.mk
+++ properties.mk
@@ -354,6 +354,7 @@ persist.vendor.radio.manual_nw_rej_ct=1 \
 persist.vendor.radio.procedure_bytes=SKIP \
 persist.vendor.radio.rat_on=combine \
 persist.vendor.radio.sib16_support=1 \
+persist.radio.add_power_save=1 \
 ro.telephony.default_cdma_sub=0
 
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
