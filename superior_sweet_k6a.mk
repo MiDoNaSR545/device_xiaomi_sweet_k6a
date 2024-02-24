c0afc759c364a13a2649573667eaea277b6d48ba
MiDoNaSR545 <ahmednasr1973913@gmail.com>
2024-02-24 15:23:35 +0000

    sweet_k6a: Superior Extended Bringup

--- superior_sweet_k6a.mk
+++ superior_sweet_k6a.mk
@@ -12,12 +12,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/sweet_k6a/device.mk)
 
 # Inherit some common LineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/superior/config/common.mk)
 
-PRODUCT_NAME := lineage_sweet_k6a
+# Superior flags
+TARGET_SUPPORTS_BLUR := true
+SYSTEM_OPTIMIZE_JAVA := true
+SYSTEMUI_OPTIMIZE_JAVA := true
+TARGET_BOOT_ANIMATION_RES := 1080
+BUILD_WITH_GAPPS := true
+SUPERIOR_OFFICIAL := true
+
+PRODUCT_NAME := superior_sweet_k6a
 PRODUCT_DEVICE := sweet_k6a
 PRODUCT_BRAND := Redmi
-PRODUCT_MODEL := Redmi Note 10 Pro
+PRODUCT_MODEL := Redmi Note 12 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
